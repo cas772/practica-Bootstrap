6ed264881d5c53fe1d5d4eb8d47358af3f79f8f4
Carlos Silva <9291cs@gmail.com>
2024-10-15 15:12:09 -0300

    commit inicial

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Maquetación con Bootstrap</title>
+</head>
+<body>
+    <header>
+        <h1>MAQUETACIÓN CON BOOTSTRAP</h1>
+    </header>
+    <section></section>
+    <footer></footer>
+</body>
+</html>
