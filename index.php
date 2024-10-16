6657baac86fa78f84a4a195b54fec5c1185fa637
Carlos Silva <9291cs@gmail.com>
2024-10-16 16:44:58 -0300

    Incluyendo archivos de bootstrap

--- index.php
+++ index.php
@@ -4,12 +4,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Maquetación con Bootstrap</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 <body>
+<div class="container">
+    <div class="row">
+    <div class="col-md-12 text-primary --bs-warning" style="background-color:#f00">
     <header>
         <h1>MAQUETACIÓN CON BOOTSTRAP</h1>
     </header>
+    <div class="row">
+      <div class="col-xl-4"  style="background-color:#f00">
+        <h1>Columna 1</h1> 
+        <p>Esto es una prueba de bootstrap en xl.</p>
+      </div>
+      <div class="col-xl-4"  style="background-color:#0f0">
+        <h1>Columna 2</h1> 
+        <p>Esto es una prueba de bootstrap en xl.</p>
+      </div>
+      <div class="col-xl-4"  style="background-color:#00f">
+        <h1>Columna 3</h1> 
+        <p>Esto es una prueba de bootstrap en xl.</p>
+      </div>
+    </div>
+    </div>
     <section></section>
     <footer></footer>
+<script src="js/bootstrap.bundle.min.js"></script>
+</div>
 </body>
 </html>
